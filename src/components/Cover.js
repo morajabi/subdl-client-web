7be1893d91194418916247356736ac7476d1f6bc
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-04-22 16:45:44 +0430

    Cover with grade-js added

---
+++ src/components/Cover.js
@@ -0,0 +1,41 @@
+/* @flow */
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Grade from 'grade-js';
+
+class Cover extends Component {
+  constructor(p) {
+    super(p);
+  }
+
+  wrapper: any;
+  onImageLoad: Function;
+
+  initGrade() {
+    Grade(this.wrapper);
+  }
+
+  componentDidMount() {
+    if(this.wrapper) {
+      const img = this.wrapper.querySelector('img');
+      if (img.complete) {
+        this.initGrade();
+      } else {
+        this.onImageLoad = img.addEventListener('load', (() => {
+          this.initGrade();
+        }).bind(this));
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(this.onImageLoad);
+    this.wrapper = null;
+  }
+
+  render() {
+    return <div ref={wrapper => this.wrapper = wrapper} {...this.props} />;
+  }
+}
+
+export default Cover;
--- src/components/Subtitles.js
+++ src/components/Subtitles.js
@@ -3,25 +3,29 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import Container from './Container';
+import Cover from './Cover';
 
+// import loganPoster from '../assets/images/demo/logan-poster.jpg';
+import loganPoster from '../assets/images/demo/winter-soldier-poster.jpg';
 
 const PaddedContainer = styled(Container)`
   padding-top: 60px;
 `;
 
-const Cover = styled.div`
+const MovieCover = styled(Cover)`
   with: 100%;
   min-height: 450px;
   background: #000;
 `;
 
+
 const Subtitles = (props: any) => (
   <div {...props}>
-    <Cover>
+    <MovieCover>
       <PaddedContainer>
-
+        <img src={loganPoster} width="200px" />
       </PaddedContainer>
-    </Cover>
+    </MovieCover>
   </div>
 );
 
