de340d8996f7a7155c0f0b3b618cc155b2550908
Behnam <b.rajabifard@gmail.com>
2017-08-11 13:10:56 +0430

    Add Register

---
+++ src/components/register.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
+import { media } from '../utils/styleUtils';
+
+import Container from './Container';
+import TopNav from './TopNav';
+import Input from './Input';
+
+const PaddedContainer = styled(Container)`
+  padding-top: 100px;
+`;
+
+
+const Register = () => (
+  <div>
+    <span>sssss</span>
+  </div>
+);
+
+export default Register;
