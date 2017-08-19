d9034aefe3bc5151fc77d1099180b5315572c3a1
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-08-19 13:50:35 +0430

    feat: add search redux module

---
+++ src/modules/search.js
@@ -0,0 +1,31 @@
+export const SEARCH_QUERY_CHANGED = "counter/INCREMENT";
+
+
+// - state
+type State = {
+  searchQuery: string
+};
+
+const initialState: State = {
+  searchQuery: '',
+};
+
+// - reducer
+export default (state = initialState, action?): State => {
+  switch (action.type) {
+    case SEARCH_QUERY_CHANGED:
+      return { ...state, searchQuery: action.value };
+
+    default:
+      return state;
+  }
+};
+
+// - action creators
+export const setSearchValue = (value) => ({
+  type: SEARCH_QUERY_CHANGED,
+  value,
+})
+
+// - selectors
+export const searchQuerySelector = state => state.searchQuery;
---
+++ src/modules/tests/search.test.js
@@ -0,0 +1,48 @@
+import reducer, {
+  SEARCH_QUERY_CHANGED,
+  // action creators
+  setSearchValue,
+  // selectors
+  searchQuerySelector,
+} from '../search';
+
+describe('Search [redux module]', () => {
+  describe('action creators', () => {
+    describe('setSearchValue', () => {
+      it('should generate correct action', () => {
+        expect(setSearchValue('query')).toEqual({
+          type: SEARCH_QUERY_CHANGED,
+          value: 'query',
+        });
+      });
+    });
+  });
+
+  describe('selectors', () => {
+    describe('searchQuerySelector', () => {
+      it('should return correct value from state', () => {
+        const state = { searchQuery: 'query' };
+
+        expect(searchQuerySelector(state)).toBe('query');
+      });
+    });
+  });
+
+  describe('reducer', () => {
+    it('should return correct initial state', () => {
+      expect(reducer(undefined, {})).toEqual({
+        searchQuery: '',
+      });
+    });
+
+    it('should change searchQuery value correctly on appropriate action', () => {
+      const action = {
+        type: SEARCH_QUERY_CHANGED,
+        value: 'new query',
+      };
+      const nextState = { searchQuery: 'new query' };
+
+      expect(reducer(undefined, action)).toEqual(nextState);
+    });
+  });
+});
