28c08ead7ae943e6327d66bda026d1034ae9db20
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-08-21 23:57:35 +0430

    feat: make Badge component

---
+++ src/components/Badge.js
@@ -0,0 +1,15 @@
+/* @flow */
+import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
+
+const Badge = styled.div`
+  padding: 3px 7px;
+  letter-spacing: .25px;
+  font-size: 12px;
+  font-weight: bold;
+  border-radius: 2px;
+  background: #999;
+  color: #f2f2f2;
+`;
+
+export default Badge;
