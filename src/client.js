09f4ea06980860315b8e3811d10b1b11ab843533
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-08-26 16:43:36 +0430

    feat: enable query batching in apollo network interface

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo';
+import { ApolloClient, ApolloProvider, createBatchingNetworkInterface } from 'react-apollo';
 import { AppContainer } from 'react-hot-loader';
 import createStore from './store';
 
@@ -13,8 +13,10 @@ import App from './components/App';
 
 // Init Apollo
 const apolloClient = new ApolloClient({
-  networkInterface: createNetworkInterface({
+  networkInterface: createBatchingNetworkInterface({
     uri: 'http://localhost:3000/graphql',
+    batchInterval: 15,
+    batchMax: 15,
   }),
 });
 
