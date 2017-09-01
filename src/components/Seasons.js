c75f8c4f84ae70ea200bdcf10679c875bc018d49
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-09-01 20:34:36 +0430

    feat: make episodes scrollable with arrows

--- src/components/Seasons.js
+++ src/components/Seasons.js
@@ -5,6 +5,7 @@ import { colors, media } from '../utils/styleUtils';
 
 import Scrollbars from 'react-custom-scrollbars';
 import Container from './Container';
+import { ResetedButton } from './Button';
 
 const Wrapper = styled.div`
   padding: 13px;
@@ -18,7 +19,7 @@ const SeasonsBar = styled.div`
   font-size: 24px;
 
   ${media.to.small`
-    padding-bottom: 15px;  
+    padding-bottom: 15px;
   `}
 `;
 
@@ -58,11 +59,57 @@ const OtherSeasonsLink = styled.div`
   }
 `;
 
+const EpisodesScrollContainer = styled.div`
+  position: relative;
+`;
+
 const Episodes = styled.div`
   display: inline-block;
   position: relative;
   white-space: nowrap;
   overflow: hidden;
+`;
+
+const Shadow = styled(ResetedButton)`
+  height: 55px;
+  width: 35px;
+  position: absolute;
+  top: 0;
+  cursor: pointer;
+  transition: transform .1s;
+
+  &:hover,
+  &:focus {
+    transform: scale(1.15);
+  }
+
+  &:after {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 1.7em;
+    font-weight: bold;
+    color: #fff;
+  }
+`;
+
+const RightShadow = styled(Shadow)`
+  right: 0;
+  background: linear-gradient(90deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4));
+
+  &:after {
+    content: '→';
+  }
+`;
+
+const LeftShadow = styled(Shadow)`
+  left: 0;
+  background: linear-gradient(90deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.3));
+
+  &:after {
+    content: '←';
+  }
 `;
 
 const Episode = styled.div`
@@ -88,8 +135,8 @@ const Episode = styled.div`
     opacity: .8;
   }
 
-  &:last-item { 
-    margin-right: 0; 
+  &:last-item {
+    margin-right: 0;
   }
 
   &:hover {
@@ -104,14 +151,28 @@ const ReleaseDate = styled.time`
   opacity: 0.6;
 `;
 
+type State = {
+  showRight: boolean,
+  showLeft: boolean,
+};
 
-// const Seasons = ({ ...props }: { props: any }) => (
+class Seasons extends Component<{}, State> {
 
-class Seasons extends Component {
   scrollbar: any;
-  
-  constructor(props: Object) {
-    super(props);
+  moveLeft: Function;
+  moveRight: Function;
+  scrollUpdated: Function;
+
+  constructor(...args: any) {
+    super(...args);
+    this.moveLeft = this.moveLeft.bind(this);
+    this.moveRight = this.moveRight.bind(this);
+    this.scrollUpdated = this.scrollUpdated.bind(this);
+  }
+
+  state = {
+    showRight: true,
+    showLeft: false,
   }
 
   componentDidMount() {
@@ -120,47 +181,89 @@ class Seasons extends Component {
   }
 
   render() {
+    const { showRight, showLeft } = this.state;
+
     return (
-      <Wrapper {...this.props}>
+      <Wrapper>
         <Container>
+
           <SeasonsBar>
             <SeasonsBarTitle>Choose From</SeasonsBarTitle>
             <Season>Season 1</Season>
             <OtherSeasonsLink>other seasons →</OtherSeasonsLink>
           </SeasonsBar>
-          {/* activate unversal with `universal` prop */}
-          <Scrollbars 
-            thumbVertical={false}
-            style={{ height: 55 }} 
-            ref={r => this.scrollbar = r} 
-            renderTrackVertical={props => <div {...props} className="track-vertical" style={{display:"none"}}/>}
-            renderThumbVertical={props => <div {...props} className="thumb-vertical" style={{display:"none"}}/>}
-          >
-            <Episodes>
-              <Episode>01</Episode>
-              <Episode>02</Episode>
-              <Episode>03</Episode>
-              <Episode>04</Episode>
-              <Episode>05</Episode>
-              <Episode>06</Episode>
-              <Episode>07</Episode>
-              <Episode>08</Episode>
-              <Episode>09</Episode>
-              <Episode>10</Episode>
-              <Episode>11</Episode>
-              <Episode>12</Episode>
-              <Episode>13</Episode>
-              <Episode>14</Episode>
-              <Episode>15</Episode>
-              <Episode>16</Episode>
-              <Episode>17</Episode>
-              <Episode>18 <ReleaseDate>7 days ago</ReleaseDate></Episode>
-              <Episode selected>19 <ReleaseDate>yesterday</ReleaseDate></Episode>
-            </Episodes>
-          </Scrollbars>
+
+          <EpisodesScrollContainer>
+            {/* activate unversal with `universal` prop */}
+            <Scrollbars
+              thumbVertical={false}
+              style={{ height: 60, transition: 'all .2s' }}
+              ref={r => this.scrollbar = r}
+              onScrollFrame={this.scrollUpdated}
+              renderTrackVertical={props =>
+                <div
+                  {...props}
+                  className="track-vertical"
+                  style={{display:"none"}}
+                />
+              }
+              renderThumbVertical={props =>
+                <div {...props}
+                  className="thumb-vertical"
+                  style={{display:"none"}}
+                />
+              }
+            >
+              <Episodes>
+                <Episode>01</Episode>
+                <Episode>02</Episode>
+                <Episode>03</Episode>
+                <Episode>04</Episode>
+                <Episode>05</Episode>
+                <Episode>06</Episode>
+                <Episode>07</Episode>
+                <Episode>08</Episode>
+                <Episode>09</Episode>
+                <Episode>10</Episode>
+                <Episode>11</Episode>
+                <Episode>12</Episode>
+                <Episode>13</Episode>
+                <Episode>14</Episode>
+                <Episode>15</Episode>
+                <Episode>16</Episode>
+                <Episode>17</Episode>
+                <Episode>18 <ReleaseDate>7 days ago</ReleaseDate></Episode>
+                <Episode selected>19 <ReleaseDate>yesterday</ReleaseDate></Episode>
+              </Episodes>
+            </Scrollbars>
+
+            {showRight && <RightShadow onClick={this.moveLeft} />}
+            {showLeft && <LeftShadow onClick={this.moveRight} />}
+          </EpisodesScrollContainer>
         </Container>
       </Wrapper>
     );
+  }
+
+  moveLeft() {
+    const nextPos = Number(this.scrollbar.getScrollLeft()) + 70;
+    this.scrollbar.scrollLeft(nextPos);
+  }
+
+  moveRight() {
+    const nextPos = Number(this.scrollbar.getScrollLeft()) - 70;
+    this.scrollbar.scrollLeft(nextPos);
+  }
+
+  scrollUpdated(values: Object) {
+    const { left } = values;
+    if(left === 0) {
+      this.setState({ showLeft: false, showRight: true });
+    } else if (left === 1) {
+      this.setState({ showLeft: true, showRight: false });
+    } else {
+      this.setState({ showLeft: true, showRight: true });
+    }
   }
 }
 
