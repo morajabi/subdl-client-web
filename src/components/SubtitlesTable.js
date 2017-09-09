6a97eb439d57a6e7ea0220e4cd10003d21d65d2e
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-09-09 17:52:59 +0430

    fix(table): change download btn behaviour

--- src/components/SubtitlesTable.js
+++ src/components/SubtitlesTable.js
@@ -4,6 +4,7 @@ import styled, { css } from 'styled-components';
 import rem from '../utils/rem';
 import { grey, lightYellow, lightBlue } from '../utils/colors';
 import { truncate } from '../utils/styleUtils';
+import { ResetedButton } from './Button';
 
 const rowHeight = 40;
 const rowPadding = 15;
@@ -18,7 +19,7 @@ const SubtitlesTable = props => (
       <Cell fixed width="5%">H.I.</Cell>
       <Cell fixed width="10%">owner</Cell>
       <Cell fixed width="17%">comment{/* comment */}</Cell>
-      <Cell fixed width="20%">action{/* action */}</Cell>
+      <Cell fixed width="20%">{/* action */}</Cell>
     </Head>
 
     <Row>
@@ -34,14 +35,17 @@ sasdasdasd
       <Cell fixed width="17%">
         <Comment>Sync. WEB-DL & HDRips</Comment>
       </Cell>
-      <Cell fixed width="20%">action</Cell>
+      <Cell fixed width="20%">
+        <Download>download</Download>
+      </Cell>
     </Row>
+
 
     <Row>
       <Cell fixed width="3%">1</Cell>
       <Cell fixed width="15%">Persian</Cell>
       <Cell fixed width="30%">
-        sasdasdasd
+sasdasdasd
       </Cell>
       <Cell fixed width="5%">H.I.</Cell>
       <Cell fixed width="10%">
@@ -50,7 +54,9 @@ sasdasdasd
       <Cell fixed width="17%">
         <Comment>Sync. WEB-DL & HDRips</Comment>
       </Cell>
-      <Cell fixed width="20%">action</Cell>
+      <Cell fixed width="20%">
+        <Download>download</Download>
+      </Cell>
     </Row>
   </Wrapper>
 );
@@ -67,6 +73,10 @@ const Row = styled.div`
 
   min-height: ${rem(rowHeight)};
   padding: 0 ${rem(rowPadding)};
+
+  &:hover [data-label="download"] {
+    opacity: 1;
+  }
 
   &:nth-child(n+3) {
     background: ${lightYellow};
@@ -104,7 +114,7 @@ const Owner = styled.a`
   font-size: .9em;
   letter-spacing: ${rem(0.4)};
   cursor: pointer;
-  color: ${lightBlue};
+  color: #555;
 
   &:hover {
     text-decoration: underline;
@@ -116,6 +126,21 @@ const Comment = styled.div`
   ${truncate(`calc(100% - ${rem(cellPadding)})`)}
 `;
 
-const Download = styled.div`
-  ${truncate(`calc(100% - ${rem(cellPadding)})`)}
+const Download = styled(ResetedButton).attrs({
+  'data-label': 'download',
+})`
+  display: inline-block;
+  padding: ${rem(4)} ${rem(6)};
+  font-weight: 400;
+  font-size: .9em;
+
+  color: ${lightBlue};
+  background: rgba(0, 0, 0, 0.04);
+  opacity: 0.8;
+  cursor: pointer;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.08);
+    color: ${lightBlue};
+  }
 `;
